4359757fb6dab3a06219bdd3f8084f7a38a230e7
Jose <Jlrios@nevada.unr.edu>
2019-05-18 22:33:18 -0700

    Finished responsive nav

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,15 @@ import '../../scss/styles.scss';
 const btn = document.querySelector('.nav__btn');
 btn.addEventListener('click', toggleNav);
 const lines = document.querySelectorAll('.line');
+const nav = document.querySelector('nav');
+const links = document.querySelectorAll('.link');
+links.forEach((link) =>
+	link.addEventListener('click', function () {
+		nav.classList = 'show';
+	})
+);
 
 function toggleNav (x) {
 	lines.forEach((line) => line.classList.toggle('change'));
+	nav.classList.toggle('show');
 }
